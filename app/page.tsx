e9dca69eac452b0715fcbe4d2b272dbb18051477
Gustavo Diniz <joaogustavone@gmail.com>
2024-11-02 01:38:08 +0000

    adding logo to page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <Image
         className="dark:invert"
-        src="https://pipley-farm.com/resources/Pipley%20Bottom%20Farm%20Copyrighted%20Logo%20May%2018.JPG.opt402x189o0%2C0s402x189.JPG"
+        src="/logo.jpg"
         alt="Pipley Bottom Farm"
         width={402}
         height={189}
       />
-      <p className="mt-4 text-xl font-semibold">Under Construction</p>
+      <p className="mt-4 text-sm font-semibold">Under Development...</p>
     </div>
   );
 }
