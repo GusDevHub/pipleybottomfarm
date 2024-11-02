d849efd531ffa100961aff4fd70e75c7dab0ce35
Gustavo Diniz <joaogustavone@gmail.com>
2024-11-02 01:16:16 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // next.config.js
-
-module.exports = {
+const nextConfig = {
   images: {
-    domains: ['https://pipley-farm.com/'],
+    remotePatterns: [
+      {
+        hostname: 'utfs.io',
+      },
+      {
+        hostname: 'https://pipley-farm.com',
+      },
+    ],
   },
-};
-};
+}
 
-export default nextConfig;
+export default nextConfig
