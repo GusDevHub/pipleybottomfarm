7d2ad60a1c9d33b3026b1f934f0ad10d98d98efe
Gustavo Diniz <joaogustavone@gmail.com>
2024-11-03 17:50:32 +0000

    add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,19 @@
+const ContactPage = () => {
+  return (
+    <div className="max-w-screen-lg mx-auto p-3">
+      <h1 className="font-bold text-green-700">Contact</h1>
+      <p className="py-3">
+        Local products for Local people A small farm of 42 acres offering
+        agricultural services, livestock husbandry/ herd management, Stud
+        services and Mare in foal, supply of hay, straw, haylage, firewood plus
+        forestry services including coppicing for &apos;Bio mass&apos; and hedgerow
+        laying.
+      </p>
+      <p className="py-3">
+        We also offer wild camping and almost wild camping.
+      </p>
+    </div>
+  );
+};
+
+export default ContactPage;
