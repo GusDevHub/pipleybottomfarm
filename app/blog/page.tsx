b4ae37198c45e80fc6b381e0917c8880a0483723
Joaogdf <joaogustavone@gmail.com>
2024-11-03 02:18:31 +0000

    blog page edition

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,11 +3,7 @@ const BlogPage = () => {
     <div className="max-w-screen-lg mx-auto p-3">
       <h1 className="font-bold text-green-700">Blog</h1>
       <p className="py-3">
-        Local products for Local people A small farm of 42 acres offering
-        agricultural services, livestock husbandry/ herd management, Stud
-        services and Mare in foal, supply of hay, straw, haylage, firewood plus
-        forestry services including coppicing for &apos;Bio mass&apos; and hedgerow
-        laying.
+        Welcome to our blog
       </p>
       <p className="py-3">
         We also offer wild camping and almost wild camping.
