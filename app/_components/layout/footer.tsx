309e3fc112eda87645175d91ea1631edcd5f2ded
Joaogdf <joaogustavone@gmail.com>
2024-11-04 00:19:19 +0000

    fixing footer addres size

--- app/_components/layout/footer.tsx
+++ app/_components/layout/footer.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
       <Card className="w-full bg-blue-950 rounded-none border-none">
         <CardHeader className="text-sm text-white">
           <div className="flex flex-col space-y-3 mx-auto max-w-screen-lg w-full">
-            <div className="flex items-start justify-start gap-1">
+            <div className="flex items-middle justify-start gap-1">
               <MapPin size={16} className="inline-block text-yellow-400" />
-              <p className="text-xs sm:text-sm">
+              <p className="text-[10px] sm:text-sm">
                 North Stoke Lane, Upton Cheyney, Bristol BS30 6NG, United
                 Kingdom
               </p>
             </div>
-            <div className="flex items-start justify-start gap-1">
+            <div className="flex items-middle justify-start gap-1">
               <Phone size={16} className="inline-block text-yellow-400" />
               <p>0117 9570 100</p>
             </div>
