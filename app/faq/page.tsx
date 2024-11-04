3d0f4eb63cb8d5d2fb36237704c4f02ed31ad1fd
Joaogdf <joaogustavone@gmail.com>
2024-11-04 04:11:56 +0000

    adding accordion and faq sample content

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,13 +1,32 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../_components/ui/accordion";
+
 const FAQPage = () => {
-    return (
-      <div>
-        <h1 className="font-bold text-green-700">Frequently Asked Questions</h1>
-        <p className="py-3">
-          Awating content...
-        </p>
+  return (
+    <div>
+      <h1 className="font-bold text-green-700">Frequently Asked Questions</h1>
+      <div className="py-3">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is this a sample question?</AccordionTrigger>
+            <AccordionContent>
+              Yes. This is the sample answer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
-    );
-  };
-  
-  export default FAQPage;
-  
+    </div>
+  );
+};
+
+export default FAQPage;
