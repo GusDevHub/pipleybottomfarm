b1380cb7f6ae7038e05a6b5f7c43a8d5843a4109
Joaogdf <joaogustavone@gmail.com>
2024-11-10 03:28:05 +0000

    amending home

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import { format } from "date-fns";
 import { enGB } from "date-fns/locale";
 
-export default function Home({ currentDate }) {
+export default function Home() {
   return (
     <>
       <div className="mx-auto max-w-screen-lg">
         <h2 className="font-semibold">Welcome back!</h2>
-        <p className="text-sm text-green-900">{currentDate}</p>
+        <p className="text-sm text-green-900">
+          {format(new Date(), "EEEE, d MMMM yyyy", { locale: enGB })}
+        </p>
       </div>
     </>
   );
-}
-
-export async function getServerSideProps() {
-  const currentDate = format(new Date(), "EEEE, d MMMM yyyy", { locale: enGB });
-  return { props: { currentDate } };
 }
