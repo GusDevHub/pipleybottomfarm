b434ec85dce8679d68b1d1386fe645c202012319
Gustavo Diniz <joaogustavone@gmail.com>
2024-11-10 19:36:54 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { format } from "date-fns";
 import { enGB } from "date-fns/locale";
 
-export default function Home() {
+export default function Home({ currentDate }) {
   return (
     <>
       <div className="mx-auto max-w-screen-lg">
         <h2 className="font-semibold">Welcome back!</h2>
-        <p className="text-sm text-green-900">{format(new Date(), "EEEE, d MMMM yyyy", {locale: enGB})}</p>
+        <p className="text-sm text-green-900">{currentDate}</p>
       </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const currentDate = format(new Date(), "EEEE, d MMMM yyyy", { locale: enGB });
+  return { props: { currentDate } };
 }
