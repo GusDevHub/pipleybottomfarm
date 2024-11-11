39e5086b75205913b211bcb56dc46ef35aa21fae
Joaogdf <joaogustavone@gmail.com>
2024-11-11 02:51:45 +0000

    adding services to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import products from "./_data/products";
 import { Card, CardContent, CardHeader } from "./_components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import services from "./_data/services";
 
 export default function Home() {
   return (
     <>
       <div className="mx-auto max-w-screen-lg">
         <div>
-          <h2 className="font-semibold">Welcome back!</h2>
-          <p className="text-sm text-green-900">
+          <h2 className="text-green-800 font-semibold">Welcome</h2>
+          <p className="text-xs text-green-900">
             {format(new Date(), "EEEE, d MMMM yyyy", { locale: enGB })}
           </p>
         </div>
@@ -35,6 +36,32 @@ export default function Home() {
                   </CardHeader>
                   <CardContent className="bg-green-800/85 truncate text-white text-sm text-center font-semibold py-2 mt-32 mb-3 relative z-10">
                     {product.name}
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="my-4">
+          <h3 className="text-xs text-gray-400 font-bold uppercase mb-2">
+            Our services
+          </h3>
+          <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {services.map((service) => (
+              <Link href="/products" key={service.name}>
+                <Card
+                  className="relative min-w-[348px] rounded-2xl max-w-96 overflow-hidden"
+                >
+                  <CardHeader className="absolute top-0 left-0 w-full h-full bg-slate-500 rounded-lg z-0">
+                    <Image
+                      src={service.image}
+                      fill
+                      className="object-cover rounded-lg hover:scale-110 hover:duration-300 duration-500"
+                      alt={service.alt}
+                    />
+                  </CardHeader>
+                  <CardContent className="bg-blue-800/85 truncate text-white text-sm text-center font-semibold py-2 mt-44 mb-3 relative z-10">
+                    {service.name}
                   </CardContent>
                 </Card>
               </Link>
