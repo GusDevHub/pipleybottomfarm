ee960884857308772a57ad9e5b041220cb1c4cde
Joaogdf <joaogustavone@gmail.com>
2024-11-12 02:33:50 +0000

    added dialog and table components and improved products section

--- app/_data/products.js
+++ app/_data/products.js
@@ -1,132 +1,291 @@
 const products = [
-    {
-      name: 'Hay',
-      price: '£4.50',
-      stock: 1,
-      link: '',
-      image: '/products/hay.jpg',
-      alt: 'Hay',
-    },
-    {
-      name: 'Straw',
-      price: '£4.00',
-      stock: 1,
-      link: '',
-      image: '/products/straw.jpg',
-      alt: 'Straw',
-    },
-    {
-      name: 'Haylage',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/haylage.jpg',
-      alt: 'Haylage',
-    },
-    {
-      name: 'Kindling',
-      price: '£6.00',
-      stock: 2,
-      link: '',
-      image: '/products/kindling.jpg',
-      alt: 'Kindling',
-    },
-    {
-      name: 'Small logs',
-      price: '£7.00',
-      stock: 2,
-      link: '',
-      image: '/products/small-log.jpg',
-      alt: 'Small log',
-    },
-    {
-      name: 'Eggs',
-      price: '£2.50 (6 units)',
-      stock: 1,
-      link: '',
-      image: '/products/eggs.jpg',
-      alt: 'Eggs',
-    },
-    {
-      name: 'Fertilised eggs',
-      price: '£12.00 (6 units)',
-      stock: 1,
-      link: '',
-      image: '/products/fertilised-egg.jpg',
-      alt: 'Fertilised eggs',
-    },
-    {
-      name: 'Day old ducklings/chicks',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/duckling-chick.jpg',
-      alt: 'Day old ducklings/chicks',
-    },
-    {
-      name: 'Young Waterfowl',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/young-waterfowl.jpg',
-      alt: 'Young Waterfowl',
-    },
-    {
-      name: 'Apples',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/apples.jpg',
-      alt: 'Apples',
-    },
-    {
-      name: 'Pears',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/pears.jpg',
-      alt: 'Pears',
-    },
-    {
-      name: 'Gages, plums, damsons',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/plums.jpg',
-      alt: 'Gages, plums, damsons',
-    },
-    {
-      name: 'Cherries',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/cherries.jpg',
-      alt: 'Cherries',
-    },
-    {
-      name: 'Nuts',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/nuts.jpg',
-      alt: 'Nuts',
-    },
-    {
-      name: 'Hedgerow fruit',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/hedgerow-fruits.jpg',
-      alt: 'Hedgerow fruit',
-    },
-    {
+  {
+    name: 'Hay',
+    price: '£4.50 per small bale',
+    stock: 1,
+    description: 'May increase in line with availability and significant increases in fuel costs during 2022-23; noting our small bales remain much larger than many of those other small bales offered locally.',
+    type: [{
+      name: 'Small Bale',
+      price: '£4.50'
+    },],
+    link: '',
+    image: '/products/hay.jpg',
+    alt: 'Hay',
+  },
+  {
+    name: 'Straw',
+    price: '£4.00',
+    stock: 1,
+    description: 'May increase in line with availability during 2022.',
+    type: [{
+      name: 'Small Bale',
+      price: '£4.00'
+    },],
+    link: '',
+    image: '/products/straw.jpg',
+    alt: 'Straw',
+  },
+  {
+    name: 'Haylage',
+    price: 'Request availability info',
+    stock: 3,
+    link: '/contact',
+    image: '/products/haylage.jpg',
+    alt: 'Haylage',
+  },
+  {
+    name: 'Kindling',
+    price: '£6.00',
+    stock: 2,
+    description: 'Plastic crate circa 500mm x 300mm x 250mm detph. To reduce waste packaging firewood is delivered in carry size plastic crates, which are brought away on delivery, all fire wood is naturally seasoned and dried.',
+    type: [{
+      name: 'Twigs not processed',
+      price: '£6.00 per plastic crate'
+    },],
+    link: '',
+    image: '/products/kindling.jpg',
+    alt: 'Kindling',
+  },
+  {
+    name: 'Small logs',
+    price: '£7.00',
+    stock: 2,
+    description: 'Plastic crate circa 500mm x 300mm x 250mm detph. To reduce waste packaging firewood is delivered in carry size plastic crates, which are brought away on delivery, all fire wood is naturally seasoned and dried.',
+    type: [{
+      name: 'Naturally seasoned',
+      price: '£7.00 per plastic crate'
+    },],
+    link: '',
+    image: '/products/small-log.jpg',
+    alt: 'Small log',
+  },
+  {
+    name: 'Eggs',
+    price: '£2.50 (6 units)',
+    stock: 1,
+    description: 'Rare breed Duck and Chicken',
+    type: [{
+      name: 'Welsh Harlequin Duck Eggs',
+      price: '£2.50 per half dozen'
+    }, {
+      name: 'Golden Crested Legbar (blue eggs)',
+      price: '£1.50 per half dozen (tend to be smaller eggs)'
+    },],
+    link: '',
+    image: '/products/eggs.jpg',
+    alt: 'Eggs',
+  },
+  {
+    name: 'Fertilised eggs',
+    price: '£12.00 (6 units)',
+    stock: 1,
+    type: [{
+      name: 'Welsh Harlequin',
+      price: '£12 per half dozen'
+    }, {
+      name: 'Golden Crested Legbar',
+      price: '£10 per half dozen'
+    },],
+    link: '',
+    image: '/products/fertilised-egg.jpg',
+    alt: 'Fertilised eggs',
+  },
+  {
+    name: 'Day old ducklings/chicks',
+    price: 'Request availability info',
+    stock: 3,
+    description: 'Normally sold as a trio',
+    type: [{
+      name: 'Ducklings',
+      price: 'Contact for price'
+    }, {
+      name: 'Chicks',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/duckling-chick.jpg',
+    alt: 'Day old ducklings/chicks',
+  },
+  {
+    name: 'Young Waterfowl',
+    price: 'Request availability info',
+    stock: 3,
+    description: 'Normally sold as a trio',
+    type: [{
+      name: 'Welsh Harlequin Ducks',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/young-waterfowl.jpg',
+    alt: 'Young Waterfowl',
+  },
+  {
+    name: 'Young Chickens',
+    price: 'Request availability info',
+    stock: 3,
+    description: 'Normally sold as a trio',
+    type: [{
+      name: 'Golden Crested Leg bars',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/fertilised-egg.jpg',
+    alt: 'Young Waterfowl',
+  },
+  {
+    name: 'Apples',
+    price: 'Request availability info',
+    stock: 3,
+    type: [{
+      name: 'Ashmead Kernel',
+      price: 'Contact for price'
+    }, {
+      name: 'Morgan Sweet (For eating and Cider)',
+      price: 'Contact for price'
+    }, {
+      name: 'Early Worcester',
+      price: 'Contact for price'
+    }, {
+      name: 'Katy',
+      price: 'Contact for price'
+    }, {
+      name: 'Kingston Black (Cider)',
+      price: 'Contact for price'
+    }, {
+      name: 'Dabinet (Cider)',
+      price: 'Contact for price'
+    }, {
+      name: 'Cox Pippin',
+      price: 'Contact for price'
+    }, {
+      name: 'John O Gold',
+      price: 'Contact for price'
+    }, {
+      name: 'Orleans Reinette',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/apples.jpg',
+    alt: 'Apples',
+  },
+  {
+    name: 'Pears',
+    price: 'Request availability info',
+    stock: 3,
+    type: [{
+      name: 'Doyenne du Comice (French)',
+      price: 'Contact for price'
+    }, {
+      name: 'Williams',
+      price: 'Contact for price'
+    }, {
+      name: 'Conference',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/pears.jpg',
+    alt: 'Pears',
+  },
+  {
+    name: 'Gages, plums, damsons',
+    price: 'Request availability info',
+    stock: 3,
+    type: [{
+      name: 'Oullins Golden Gage',
+      price: 'Contact for price'
+    }, {
+      name: 'Green Gage',
+      price: 'Contact for price'
+    }, {
+      name: 'English Plum',
+      price: 'Contact for price'
+    }, {
+      name: 'Sloes (For Gin/ Preserves or Jam)',
+      price: 'Contact for price'
+    }, {
+      name: 'Victoria Plum',
+      price: 'Contact for price'
+    }, {
+      name: 'Damson',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/plums.jpg',
+    alt: 'Gages, plums, damsons',
+  },
+  {
+    name: 'Cherries',
+    price: 'Request availability info',
+    stock: 3,
+    type: [{
+      name: 'Stella',
+      price: 'Contact for price'
+    }, {
+      name: 'Morello',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/cherries.jpg',
+    alt: 'Cherries',
+  },
+  {
+    name: 'Nuts',
+    price: 'Request availability info',
+    stock: 3,
+    type: [{
+      name: 'Sweet Chestnut',
+      price: 'Contact for price'
+    }, {
+      name: 'Hazel Nut',
+      price: 'Contact for price'
+    }, {
+      name: 'Walnut',
+      price: 'Contact for price'
+    }, {
+      name: 'Giant Cob Nut',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/nuts.jpg',
+    alt: 'Nuts',
+  },
+  {
+    name: 'Hedgerow fruit',
+    price: 'Request availability info',
+    stock: 3,
+    type: [{
+      name: 'Sloes',
+      price: 'Contact for price'
+    }, {
+      name: 'Hornbeam Berries',
+      price: 'Contact for price'
+    }, {
+      name: 'Elderflower',
+      price: 'Contact for price'
+    }, {
+      name: 'Elder Berries',
+      price: 'Contact for price'
+    }, {
+      name: 'Rowan berries',
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/hedgerow-fruits.jpg',
+    alt: 'Hedgerow fruit',
+  },
+  {
+    name: 'British saanen goats cheese',
+    price: 'Request availability info',
+    stock: 3,
+    description: 'Available at markets',
+    type: [{
       name: 'British saanen goats cheese',
-      price: 'Request availability info',
-      stock: 3,
-      link: '/contact',
-      image: '/products/goat-cheese.jpg',
-      alt: 'British saanen goats cheese',
-    },
-  ];
-  
-  export default products;  
+      price: 'Contact for price'
+    },],
+    link: '/contact',
+    image: '/products/goat-cheese.jpg',
+    alt: 'British saanen goats cheese',
+  },
+];
+
+export default products;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -8,8 +8,40 @@ import {
 import products from "../_data/products";
 import { Badge } from "../_components/ui/badge";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../_components/ui/dialog";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../_components/ui/table";
 
 type StockStatus = number | undefined;
+
+type ProductType = {
+  name: string;
+  price: string;
+};
+
+type Product = {
+  name: string;
+  price: string;
+  stock: StockStatus;
+  link?: string;
+  image: string;
+  alt: string;
+  type?: ProductType[];
+  description?: string;
+};
 
 const getStockStatus = (stock: StockStatus) => {
   switch (stock) {
@@ -30,52 +62,103 @@ const ProductsPage = () => {
       <h1 className="font-bold text-green-700 border-b pb-3">Products</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 w-full">
         {products.map((product) => (
-          <Card
-            key={product.name}
-            className="relative w-full max-w-96 overflow-hidden"
-          >
-            <CardHeader className="absolute top-0 left-0 w-full h-full bg-slate-500 rounded-lg z-0">
-              <Image
-                src={product.image}
-                fill
-                className="object-cover rounded-lg hover:scale-110 hover:duration-300 duration-500"
-                alt={product.alt}
-              />
-              <CardTitle className="absolute left-2 bottom-2 w-full pr-4">
-                <div className="flex items-center justify-center gap-1">
-                {product.stock && (
-                  <Badge
-                    className={`text-white w-fit ${
-                      getStockStatus(product.stock).style
-                    } hover:${getStockStatus(product.stock).style}`}
-                  >
-                    {getStockStatus(product.stock).text}
-                  </Badge>
+          <Dialog key={product.name}>
+            <DialogTrigger>
+              <Card className="relative w-full max-w-96 overflow-hidden">
+                <CardHeader className="absolute top-0 left-0 w-full h-full bg-slate-500 rounded-lg z-0">
+                  <Image
+                    src={product.image}
+                    fill
+                    className="object-cover rounded-lg hover:scale-110 hover:duration-300 duration-500"
+                    alt={product.alt}
+                    sizes="(max-width: 768px) 100vw, 50vw"
+                  />
+                  <CardTitle className="absolute left-2 bottom-2 w-full pr-4">
+                    <div className="flex items-center justify-center gap-1">
+                      {product.stock && (
+                        <Badge
+                          className={`text-white w-fit ${
+                            getStockStatus(product.stock).style
+                          } hover:${getStockStatus(product.stock).style}`}
+                        >
+                          {getStockStatus(product.stock).text}
+                        </Badge>
+                      )}
+                      {product.price && (
+                        <Badge
+                          className={`w-fit ${
+                            product.price.startsWith("£")
+                              ? "bg-blue-500 hover:bg-blue-500 text-white"
+                              : "bg-yellow-400 hover:bg-green-500 text-yellow-800"
+                          } hover:text-white`}
+                        >
+                          {product.link ? (
+                            <Link
+                              href={product.link}
+                              className="hover:text-white"
+                            >
+                              {product.price}
+                            </Link>
+                          ) : (
+                            product.price
+                          )}
+                        </Badge>
+                      )}
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="bg-green-800/85 text-white text-sm text-center font-semibold py-2 mt-32 mb-10 relative z-10">
+                  {product.name}
+                </CardContent>
+              </Card>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>
+                  {product.name}
+                  {product.stock && (
+                    <Badge
+                      className={`text-white w-fit ml-3 ${
+                        getStockStatus(product.stock).style
+                      } hover:${getStockStatus(product.stock).style}`}
+                    >
+                      {getStockStatus(product.stock).text}
+                    </Badge>
+                  )}
+                </DialogTitle>
+                {product.description && (
+                  <DialogDescription>{product.description}</DialogDescription>
                 )}
-                {product.price && (
-                  <Badge
-                    className={`w-fit ${
-                      product.price.startsWith("£")
-                        ? "bg-blue-500 hover:bg-blue-500 text-white"
-                        : "bg-yellow-400 hover:bg-green-500 text-yellow-800"
-                    } hover:text-white`}
-                  >
-                    {product.link ? (
-                      <Link href={product.link} className="hover:text-white">
-                        {product.price}
-                      </Link>
-                    ) : (
-                      product.price
-                    )}
-                  </Badge>
+              </DialogHeader>
+              <div>
+                {product.type && (
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[250]">Product Name</TableHead>
+                        <TableHead className="text-right">Price</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {product.type.map((type) => (
+                        <TableRow key={type.name}>
+                          <TableCell className="font-medium">
+                            {type.name}
+                          </TableCell>
+                          <TableCell className="text-right">
+                            {type.price.startsWith("Contact") ? (
+                              <Link href="/contact" className="text-blue-500 underline">Contact for price</Link>
+                            ) : (<span className="text-green-600 font-medium">{type.price}</span>
+                            )}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
                 )}
-                </div>
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="bg-green-800/85 text-white text-sm text-center font-semibold py-2 mt-32 mb-10 relative z-10">
-              {product.name}
-            </CardContent>
-          </Card>
+              </div>
+            </DialogContent>
+          </Dialog>
         ))}
       </div>
     </div>
